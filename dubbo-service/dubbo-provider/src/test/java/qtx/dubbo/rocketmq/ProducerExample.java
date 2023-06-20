00c49e2a4bd673cddb359b30167938a595fbc328
QQQtx <1102214883@qq.com>
2023-06-21 00:05:34 +0800

    feat:RocketMQ test

---
+++ dubbo-service/dubbo-provider/src/test/java/qtx/dubbo/rocketmq/ProducerExample.java
@@ -0,0 +1,62 @@
+package qtx.dubbo.rocketmq;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.rocketmq.client.apis.ClientConfiguration;
+import org.apache.rocketmq.client.apis.ClientConfigurationBuilder;
+import org.apache.rocketmq.client.apis.ClientException;
+import org.apache.rocketmq.client.apis.ClientServiceProvider;
+import org.apache.rocketmq.client.apis.message.Message;
+import org.apache.rocketmq.client.apis.producer.Producer;
+import org.apache.rocketmq.client.apis.producer.SendReceipt;
+import org.apache.rocketmq.shaded.org.slf4j.Logger;
+import org.apache.rocketmq.shaded.org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+/**
+ * @author qtx
+ * @since 2023/6/20 22:50
+ */
+@Slf4j
+public class ProducerExample {
+
+//    private static final Logger logger = LoggerFactory.getLogger(ProducerExample.class);
+
+    public static void main(String[] args) throws ClientException {
+        // 接入点地址，需要设置成Proxy的地址和端口列表，一般是xxx:8081;xxx:8081。
+        String endpoint = "172.16.6.77:8081";
+        // 消息发送的目标Topic名称，需要提前创建。
+        String topic = "test";
+        ClientServiceProvider provider = ClientServiceProvider.loadService();
+        ClientConfigurationBuilder builder = ClientConfiguration.newBuilder().setEndpoints(endpoint);
+        ClientConfiguration configuration = builder.build();
+        // 初始化Producer时需要设置通信配置以及预绑定的Topic。
+        Producer producer = provider.newProducerBuilder()
+                .setTopics(topic)
+                .setClientConfiguration(configuration)
+                .build();
+        // 普通消息发送。
+        Message message = provider.newMessageBuilder()
+                .setTopic(topic)
+                // 设置消息索引键，可根据关键字精确查找某条消息。
+                .setKeys("messageKey12321213")
+                // 设置消息Tag，用于消费端根据指定Tag过滤消息。
+                .setTag("messageTag")
+                // 消息体。
+                .setBody("messageBody".getBytes())
+                .build();
+        try {
+            // 发送消息，需要关注发送结果，并捕获失败等异常。
+            SendReceipt sendReceipt = producer.send(message);
+            log.info("Send message successfully, messageId={}", sendReceipt.getMessageId());
+        } catch (ClientException e) {
+            log.error("Failed to send message", e);
+        }
+        try {
+            producer.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
