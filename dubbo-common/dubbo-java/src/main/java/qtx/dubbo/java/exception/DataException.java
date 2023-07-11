ece59fbe6751cc78e30b91f38a8697eeaf8a0976
QQQtx <1102214883@qq.com>
2023-07-11 18:00:56 +0800

    feat:全局异常处理
    自定义异常去除无用信息

--- dubbo-common/dubbo-java/src/main/java/qtx/dubbo/java/exception/DataException.java
+++ dubbo-common/dubbo-java/src/main/java/qtx/dubbo/java/exception/DataException.java
@@ -10,34 +10,11 @@ import qtx.dubbo.java.enums.DataEnums;
  */
 public class DataException {
 
-    private final String msg;
-
-    private final int code;
-
-    private final DataEnums dataEnums;
-
     public DataException(DataEnums dataEnums) {
-        this.code = dataEnums.getCode();
-        this.dataEnums = dataEnums;
         throw new RpcException(dataEnums.getCode(), dataEnums.toString());
     }
 
     public DataException(String msg, int code) {
-        this.code = code;
-        this.dataEnums = null;
         throw new RpcException(code, msg);
-    }
-
-    public int getCode() {
-        return code;
-    }
-
-    public DataEnums getDataEnums() {
-        return dataEnums;
-    }
-
-    @Override
-    public String toString() {
-        return "DataException{" + "code=" + code + ", msg='" + this.msg + '}';
     }
 }
