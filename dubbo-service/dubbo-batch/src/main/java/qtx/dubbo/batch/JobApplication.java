6183d169e372ac99fbbaf2166e646b594aa9cd9b
QQQtx <1102214883@qq.com>
2023-09-26 17:59:01 +0800

    feat:batch

---
+++ dubbo-service/dubbo-batch/src/main/java/qtx/dubbo/batch/JobApplication.java
@@ -0,0 +1,11 @@
+package qtx.dubbo.batch;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author qtx
+ * @since 2023/9/26
+ */
+@SpringBootApplication(scanBasePackages = "qtx.dubbo")
+public class JobApplication {
+}
---
+++ dubbo-service/dubbo-batch/src/main/java/qtx/dubbo/batch/config/BatchConfiguration.java
@@ -0,0 +1,13 @@
+package qtx.dubbo.batch.config;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author qtx
+ * @since 2023/9/26
+ */
+@Configuration
+@EnableBatchProcessing
+public class BatchConfiguration {
+}
