e14256d96b6d430120c12fc000a5915d1061a212
QQQtx <1102214883@qq.com>
2024-05-16 14:35:16 +0800

    feat:调整pom

--- dubbo-common/dubbo-mybatis/src/main/java/qtx/dubbo/mybatis/config/MyMetaObjectHandler.java
+++ dubbo-common/dubbo-mybatis/src/main/java/qtx/dubbo/mybatis/config/MyMetaObjectHandler.java
@@ -7,6 +7,8 @@ import org.apache.ibatis.reflection.MetaObject;
 import org.springframework.stereotype.Component;
 import qtx.dubbo.java.CommonMethod;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.time.LocalDateTime;
 
 /**
@@ -26,31 +28,34 @@ public class MyMetaObjectHandler implements MetaObjectHandler {
     @Override
     public void insertFill(MetaObject metaObject) {
         log.info("start insert fill ....");
-        String user = commonMethod.getUserCode();
-        if (StringUtils.isBlank(user)) {
-            user = commonMethod.getIp();
-            if (StringUtils.isBlank(user)){
-                user = "system";
-            }
-        }
         this.strictInsertFill(metaObject, "createOn", LocalDateTime.class, LocalDateTime.now());
-        this.strictInsertFill(metaObject, "createBy", String.class, user);
+        this.strictInsertFill(metaObject, "createBy", String.class, getUser());
         log.info("end insert fill ...");
     }
 
     @Override
     public void updateFill(MetaObject metaObject) {
         log.info("start update fill ....");
-        String user = commonMethod.getUserCode();
-        if (StringUtils.isBlank(user)) {
-            user = commonMethod.getIp();
-            if (StringUtils.isBlank(user)){
-                user = "system";
-            }
-        }
         this.strictUpdateFill(metaObject, "updateOn", LocalDateTime.class, LocalDateTime.now());
-        this.strictUpdateFill(metaObject, "updateBy", String.class, user);
+        this.strictUpdateFill(metaObject, "updateBy", String.class, getUser());
         log.info("end update fill ...");
+    }
+
+    private String getUser() {
+        try {
+            String user = commonMethod.getUserCode();
+            if (StringUtils.isBlank(user)) {
+                user = commonMethod.getIp();
+                if (StringUtils.isBlank(user)) {
+                    user = "system";
+                }
+            }
+            InetAddress address = InetAddress.getLocalHost();
+            return user + ":" + address.getHostAddress() + address.getHostName();
+        } catch (UnknownHostException e) {
+            log.warn("获取系统信息失败", e);
+        }
+        return "system";
     }
 
 }
